0bf270d3e3441367cdcd5e11cf60dbd0026f5998
YuGao <s2292976@ed.ac.uk>
2024-09-27 16:43:19 +0100

    add isCloseTo

--- src/main/java/uk/ac/ed/inf/pizzadronz/Controller.java
+++ src/main/java/uk/ac/ed/inf/pizzadronz/Controller.java
@@ -1,6 +1,6 @@
 package uk.ac.ed.inf.pizzadronz;
 
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,5 +23,21 @@ public class Controller {
         double lat2 = lnglat1.getPosition2().getLat();
 
         return Math.sqrt(Math.pow(lat2 - lat1, 2) + Math.pow(lng2 - lng1, 2));
+   }
+   @PostMapping("/isCloseTo")
+   public boolean isCloseTo(@RequestBody LngLatPairRequest lnglat1){
+        double lng1 = lnglat1.getPosition1().getLng();
+        double lng2 = lnglat1.getPosition2().getLng();
+
+        double lat1 = lnglat1.getPosition1().getLat();
+        double lat2 = lnglat1.getPosition2().getLat();
+
+        double distance = Math.sqrt(Math.pow(lat2 - lat1, 2) + Math.pow(lng2 - lng1, 2));
+
+        if (distance < 0.00015) {
+            return true;
+        }else{
+            return false;
+        }
    }
 }
